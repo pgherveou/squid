c2b7704eec3e16aa3c2eacaf6d7d42f844f8db94
PG <pgherveou@gmail.com>
2013-05-01 15:08:49 +0200

    disable postbuild

--- bin/builder.js
+++ bin/builder.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 config = require('../lib/config');
-project = require('../lib/project');
 if (process.env.DISABLE_POST_BUILD) config.post_build = {};
+
+project = require('../lib/project');
 project.buildAll();
